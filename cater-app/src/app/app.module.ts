3bae6d48f0935f5ab42eb224ba532f540996898c
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-03-23 08:32:09 -0400

    "changes"

---
+++ cater-app/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatCardModule } from '@angular/material/card';
+
+import { AppComponent } from './app.component';
+import {  FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { AgGridAngular } from 'ag-grid-angular';
+import { HttpClientModule } from '@angular/common/http';
+
+import { HomeComponent } from './Home/home/home.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+
+ const routes:Routes=[
+  {path:'',component:HomeComponent}
+]
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes),
+    AgGridAngular,
+    HttpClientModule,
+    MatCardModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule
+  ],
+  providers: [
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
